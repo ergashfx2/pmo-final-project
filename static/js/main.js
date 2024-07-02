25f56b07023ea06a88b3ef293d3164002efa8030
Abduvokhidov Ergashali <147835039+ergashfx2@users.noreply.github.com>
2024-07-02 11:06:52 +0500

    commit

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,1 @@
-document.addEventListener('DOMContentLoaded', function () {
-        var toggleButton = document.querySelector('[data-toggle="offcanvas"]');
-        var offcanvasElement = document.querySelector(toggleButton.getAttribute('data-target'));
-        var closeButton = offcanvasElement.querySelector('.close');
 
-        toggleButton.addEventListener('click', function () {
-            offcanvasElement.classList.toggle('show');
-        });
-
-        closeButton.addEventListener('click', function () {
-            offcanvasElement.classList.remove('show');
-        });
-    });
