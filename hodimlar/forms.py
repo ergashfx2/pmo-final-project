1f951add54faad31c90451ee7428087265ec32d8
Abduvokhidov Ergashali <147835039+ergashfx2@users.noreply.github.com>
2024-07-25 00:12:34 +0500

    commit

--- hodimlar/forms.py
+++ hodimlar/forms.py
@@ -6,37 +6,42 @@ from .models import User
 from hodimlar.models import Department, Blog
 
 
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
+from .models import Blog, Department
+
 class CreateUserForm(forms.ModelForm):
-    username = forms.CharField(max_length=150, label='Username kiriting')
-    first_name = forms.CharField(max_length=150, label='Ism kiriting')
-    last_name = forms.CharField(max_length=150, label='Familiya kiriting')
-    phone = forms.CharField(max_length=150, label='Telefon raqam kiriting')
-    email = forms.EmailField(label='Email kiriting')
+    username = forms.CharField(max_length=150, label='Username kiriting', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    first_name = forms.CharField(max_length=150, label='Ism kiriting', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(max_length=150, label='Familiya kiriting', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    phone = forms.CharField(max_length=150, label='Telefon raqam kiriting', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(label='Email kiriting', widget=forms.EmailInput(attrs={'class': 'form-control'}))
     role = forms.ChoiceField(
         label="Foydalanuvchi ro'lini tanlang",
         choices={
-            'Admin': 'Admin',
-            'Loyiha menejeri': "Loyiha menejeri",
-            'Loyiha egasi': "Loyiha egasi",
-            'Loyiha kuratori': 'Loyiha kuratori',
-            'O)ddiy': "Oddiy foydalanuvchi"
+            ('Admin', 'Admin'),
+            ('Loyiha menejeri', "Loyiha menejeri"),
+            ('Loyiha egasi', "Loyiha egasi"),
+            ('Loyiha kuratori', 'Loyiha kuratori'),
+            ('Oddiy', "Oddiy foydalanuvchi")
         },
         widget=forms.Select(attrs={'class': 'form-control'})
     )
-    position = forms.CharField(label='Lavozimini tanlang', max_length=150)
-    blogs = {blog.blog_name: blog.blog_name for blog in Blog.objects.all()}
-    departments = {department.department_name: department.department_name for department in Department.objects.all()}
+    position = forms.CharField(label='Lavozimini tanlang', max_length=150, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    blogs = [(blog.blog_name, blog.blog_name) for blog in Blog.objects.all()]
+    departments = [(department.department_name, department.department_name) for department in Department.objects.all()]
     blog = forms.ChoiceField(
         label='Blokni tanlang',
-        choices=blogs.items(),
+        choices=blogs,
         widget=forms.Select(attrs={'class': 'form-control'})
     )
     department = forms.ChoiceField(
         label='Departamentni tanlang',
-        choices=departments.items(),
+        choices=departments,
         widget=forms.Select(attrs={'class': 'form-control'})
     )
-    image = forms.ImageField(label="Rasm yuklang", max_length=150, required=False)
+    image = forms.ImageField(label="Rasm yuklang", max_length=150, required=False, widget=forms.FileInput(attrs={'class': 'form-control'}))
     password = forms.CharField(
         label='Parolni kiriting',
         max_length=150,
